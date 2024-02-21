59e5157049039d3927444525bc7152a939f14c8a
Reza <alirezaarifi69@gmail.com>
2024-02-21 15:21:32 +0700

    Fix Tailwind in github page (PLS WORK)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: {
-    relative: true, 
-    files: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./layout/**/*.{js,ts,jsx,tsx,mdx}",
-    ],},
+  content: ["./**/*.{html,js,tsx}"],
   theme: {
     colors: {
       'donker': '#222831',
